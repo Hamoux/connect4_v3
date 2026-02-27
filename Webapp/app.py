ebb6363a4f19c124a7ea74c59e16bae0693f6b8d
Hamou Djellab <hamoudjellab@gmail.com>
2026-02-27 14:34:53 +0100

    Refactor app.run to use environment PORT variable

--- Webapp/app.py
+++ Webapp/app.py
@@ -350,9 +350,5 @@ def api_ai_move():
     return jsonify(state)
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
-import os
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))   # Render provides PORT
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
